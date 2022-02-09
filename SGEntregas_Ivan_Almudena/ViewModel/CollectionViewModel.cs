1b0caa5edaed3a292a2700bc58657bca42690bb0
Iván Coello Cortés <ivancoellocortes@gmail.com>
2022-02-09 15:01:01 +0100

    Añadida la clase PedidosCollection y modificado el CollectionViewModel con la ListaPedidos

--- SGEntregas_Ivan_Almudena/ViewModel/CollectionViewModel.cs
+++ SGEntregas_Ivan_Almudena/ViewModel/CollectionViewModel.cs
@@ -11,25 +11,32 @@ namespace SGEntregas_Ivan_Almudena.ViewModel
 {
     public class CollectionViewModel : INotifyPropertyChanged
     {
-        public event PropertyChangedEventHandler PropertyChanged;
-        private ClientesCollection _listaClientes = new ClientesCollection();
-        private entregasEntities _objBD = new entregasEntities();
 
+        public CollectionViewModel()
+        {
+           cargarDatosClientes();
+        }
+
+        private entregasEntities _objBD = new entregasEntities();
         public entregasEntities objBD
         {
             get { return _objBD; }
             set { _objBD = value; notifyPropertyChanged(); }
         }
 
-        private void notifyPropertyChanged([CallerMemberName] string propertyName = "")
+        private PedidosCollection _listaPedidos = new PedidosCollection();
+
+        public PedidosCollection ListaPedidos
         {
-            if (PropertyChanged != null)
+            get { return _listaPedidos; }
+            set
             {
-                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+                _listaPedidos = value;
+                notifyPropertyChanged();
             }
         }
 
-       
+        private ClientesCollection _listaClientes = new ClientesCollection();
         public ClientesCollection ListaClientes
         {
             get { return _listaClientes; }
@@ -38,25 +45,39 @@ namespace SGEntregas_Ivan_Almudena.ViewModel
             }
         }
 
-        public CollectionViewModel()
-        {
-           cargarDatosClientes();
-        }
 
         private void cargarDatosClientes()
         {
             ListaClientes.Clear();
             
-            var q = from p in objBD.clientes select p;
+            var q = from p in objBD.clientes orderby p.apellidos ascending select p;
             foreach (var p in q.ToList())
             {
                 ListaClientes.Add(p);
             }            
         }
 
+        public void CargarPedidosCliente(string dni)
+        {
+            var q = from p in objBD.pedidos where p.cliente == dni select p;
+            foreach (var p in q.ToList())
+            {
+                ListaPedidos.Add(p);
+            }
+        }
+
         public void guardarDatos()
         {
             objBD.SaveChanges();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void notifyPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
         }
     }
 }
