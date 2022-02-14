7bf4509d361d420830860b7b3711954caaa95d2a
Almudena Ruedas <almudena.ruedas@gmail.com>
2022-02-14 20:02:54 +0100

    Añadida ventana seleccionar cliente desde tablet

---
+++ SGEntregas_Ivan_Almudena/Ventanas/Tablet/ClientesTablet.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SGEntregas_Ivan_Almudena.Ventanas.Tablet
+{
+    /// <summary>
+    /// Lógica de interacción para ClientesTablet.xaml
+    /// </summary>
+    public partial class ClientesTablet : Window
+    {
+        public ClientesTablet()
+        {
+            InitializeComponent();
+        }
+
+        private void btnVolver_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnMostrar_Click(object sender, RoutedEventArgs e)
+        {
+            if(listaClien.SelectedIndex != -1)
+            {
+
+            }
+            else
+            {
+                MessageBox.Show("Seleccione un cliente");
+            }
+        }
+    }
+}
