cc6431c533dc267530f7a72db1dd0c8184043bdd
Almudena Ruedas <almudena.ruedas@gmail.com>
2022-02-14 10:05:08 +0100

    Modificación comprobación dni nuevo cliente

--- SGEntregas_Ivan_Almudena/Ventanas/Escritorio/AddClientesPC.xaml.cs
+++ SGEntregas_Ivan_Almudena/Ventanas/Escritorio/AddClientesPC.xaml.cs
@@ -42,41 +42,59 @@ namespace SGEntregas_Ivan_Almudena.Ventanas.Escritorio
         }
 
         private void btnAdd_Click(object sender, RoutedEventArgs e)
-        {
-            if (txtDniCliente.Text.Length==10 && !Utils.comprobarVacios(txtNombreCliente.Text)
-                && !Utils.comprobarVacios(txtApellCliente.Text)
-                && !Utils.comprobarVacios(txtDomicilioCliente.Text)
-                && !Utils.comprobarVacios(txtLocalidadCliente.Text)                
-                && !Utils.comprobarVacios(txtEmailCliente.Text)                
-                && !Utils.comprobarVacios(cbProvin.SelectedItem.ToString()))
-            {
-                clientes objCliente = new clientes()
+        {           
+            //comprobar formato dni
+            if (Utils.validarFormatoDni(txtDniCliente.Text)){
+                //Comprueba que no exista ese dni en la bbdd
+                var cliente = cvm.objBD.clientes.Find(txtDniCliente.Text.ToUpper());
+
+                if (cliente == null)
                 {
-                    dni = txtDniCliente.Text,
-                    nombre = txtNombreCliente.Text,
-                    apellidos = txtApellCliente.Text,
-                    localidad = txtLocalidadCliente.Text,
-                    email = txtEmailCliente.Text,
-                    domicilio = txtDomicilioCliente.Text,
-                    provincia = cbProvin.SelectedIndex +1
-                    
-                };
+                   if (!Utils.comprobarVacios(txtNombreCliente.Text)
+                   && !Utils.comprobarVacios(txtApellCliente.Text)
+                   && !Utils.comprobarVacios(txtDomicilioCliente.Text)
+                   && !Utils.comprobarVacios(txtLocalidadCliente.Text)
+                   && !Utils.comprobarVacios(cbProvin.Text))
+                    {
+                        clientes objCliente = new clientes()
+                        {
+                            dni = txtDniCliente.Text,
+                            nombre = txtNombreCliente.Text,
+                            apellidos = txtApellCliente.Text,
+                            localidad = txtLocalidadCliente.Text,
+                            email = txtEmailCliente.Text,
+                            domicilio = txtDomicilioCliente.Text,
+                            provincia = cbProvin.SelectedIndex + 1
 
-                objCliente.provincia = cbProvin.SelectedIndex + 1;
-                var provin = cvm.objBD.provincias.Find(cbProvin.SelectedIndex + 1);
-                objCliente.provincias = provin;
+                        };
 
-                //Lo guarda en la lista de la tabla clientes de la conexion 
-                cvm.objBD.clientes.Add(objCliente);
-                //Lo guarda en la lista observable que tienen el binding
-                cvm.ListaClientes.Add(objCliente);
-                MessageBox.Show("Insertado correctamente");
-                this.Close();
+                        objCliente.provincia = cbProvin.SelectedIndex + 1;
+                        var provin = cvm.objBD.provincias.Find(cbProvin.SelectedIndex + 1);
+                        objCliente.provincias = provin;
+
+                        //Lo guarda en la lista de la tabla clientes de la conexion 
+                        cvm.objBD.clientes.Add(objCliente);
+                        //Lo guarda en la lista observable que tienen el binding
+                        cvm.ListaClientes.Add(objCliente);
+                        MessageBox.Show("Insertado correctamente");
+                        this.Close();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Debe rellenar los campos obligatorios", "Attention", MessageBoxButton.OK, MessageBoxImage.Warning);
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Usuario ya registrado", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+               
             }
             else
             {
-                MessageBox.Show("Debe rellenar los campos"); 
+                MessageBox.Show("El formato del dni no es correcto", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
             }
+            
         }
 
         private void btnCancel_Click(object sender, RoutedEventArgs e)
